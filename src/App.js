073abb50069d00351006ab055d33f945777fb6d9
sahaj <sahajjain13@gmail.com>
2022-03-25 19:29:36 +0530

    Add score logic in handleCardClick, Add reset function

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React, { useState } from 'react';
+import './App.css';
 import Card from './components/Card';
+import Scoreboard from './components/Scoreboard';
 import shuffleArray from './utils';
 
 function App() {
   const cardsArray = [
-    { name: 'First', id: crypto.randomUUID() },
-    { name: 'Second', id: crypto.randomUUID() },
-    { name: 'Third', id: crypto.randomUUID() },
+    { name: 'First', id: crypto.randomUUID(), isClicked: false },
+    { name: 'Second', id: crypto.randomUUID(), isClicked: false },
+    { name: 'Third', id: crypto.randomUUID(), isClicked: false },
+    { name: 'Fourth', id: crypto.randomUUID(), isClicked: false },
+    { name: 'Fifith', id: crypto.randomUUID(), isClicked: false },
+    { name: 'Sixth', id: crypto.randomUUID(), isClicked: false },
   ];
 
   const [cards, setCards] = useState(cardsArray);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
-  const handleCardClick = () => {
-    setCards(shuffleArray([...cards]));
+  const resetScore = () => {
+    setScore(0);
+    setCards(shuffleArray([...cardsArray]));
+  };
+
+  const handleCardClick = (cardId) => {
+    if (cards.find((card) => card.id === cardId).isClicked) { resetScore(); } else {
+      setScore(score + 1);
+      setBestScore(Math.max(score + 1, bestScore));
+      setCards(shuffleArray([...cards])
+        .map((card) => (card.id === cardId ? { ...card, isClicked: true } : card)));
+    }
   };
 
   const renderCards = () => cards.map((card) => (
-    <Card name={card.name} key={card.id} handleClick={handleCardClick} />
+    <Card name={card.name} key={card.id} id={card.id} handleClick={handleCardClick} />
   ));
 
   return (
     <div>
-      {renderCards()}
+      <Scoreboard score={score} bestScore={bestScore} />
+      <div className="cards-container">
+        {renderCards()}
+      </div>
     </div>
   );
 }
