1590d808eb7ff43583dda74b91da03fc64c6de66
sahaj <sahajjain13@gmail.com>
2022-03-25 23:35:08 +0530

    Add useEffect hook to populate cards from API data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 import Scoreboard from './components/Scoreboard';
-import shuffleArray from './utils';
+import fetchImages from './fetchImages';
+import { shuffleArray, sampleArrayFromRange } from './utils';
 
 function App() {
-  const cardsArray = [
-    { name: 'First', id: crypto.randomUUID(), isClicked: false },
-    { name: 'Second', id: crypto.randomUUID(), isClicked: false },
-    { name: 'Third', id: crypto.randomUUID(), isClicked: false },
-    { name: 'Fourth', id: crypto.randomUUID(), isClicked: false },
-    { name: 'Fifith', id: crypto.randomUUID(), isClicked: false },
-    { name: 'Sixth', id: crypto.randomUUID(), isClicked: false },
-  ];
+  const url = 'https://rickandmortyapi.com/api/character';
+  const idArray = sampleArrayFromRange(1, 825, 10);
+  const cardsArray = [];
 
   const [cards, setCards] = useState(cardsArray);
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
 
-  const resetScore = () => {
+  useEffect(async () => {
+    const cardsList = await fetchImages(url, idArray);
+    setCards(cardsList.map((character) => ({
+      name: character.name,
+      id: character.id,
+      image: character.image,
+      isClicked: false,
+    })));
+  }, []);
+
+  const reset = () => {
     setScore(0);
-    setCards(shuffleArray([...cardsArray]));
+    setCards(shuffleArray([...cards])
+      .map((card) => ({ ...card, isClicked: false })));
   };
 
   const handleCardClick = (cardId) => {
-    if (cards.find((card) => card.id === cardId).isClicked) { resetScore(); } else {
+    if (cards.find((card) => card.id === cardId).isClicked) { reset(); } else {
       setScore(score + 1);
       setBestScore(Math.max(score + 1, bestScore));
       setCards(shuffleArray([...cards])
@@ -33,11 +40,17 @@ function App() {
   };
 
   const renderCards = () => cards.map((card) => (
-    <Card name={card.name} key={card.id} id={card.id} handleClick={handleCardClick} />
+    <Card
+      name={card.name}
+      key={card.id}
+      id={card.id}
+      image={card.image}
+      handleClick={handleCardClick}
+    />
   ));
 
   return (
-    <div>
+    <div className="main">
       <Scoreboard score={score} bestScore={bestScore} />
       <div className="cards-container">
         {renderCards()}
