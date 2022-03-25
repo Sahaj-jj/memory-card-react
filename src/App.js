31f95fe9a622faa3743386047a0530a5359b942c
sahaj <sahajjain13@gmail.com>
2022-03-25 16:56:44 +0530

    Add App componnent to render all Cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-/* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useState } from 'react';
+import Card from './components/Card';
+import shuffleArray from './utils';
 
 function App() {
+  const cardsArray = [
+    { name: 'First', id: crypto.randomUUID() },
+    { name: 'Second', id: crypto.randomUUID() },
+    { name: 'Third', id: crypto.randomUUID() },
+  ];
+
+  const [cards, setCards] = useState(cardsArray);
+
+  const handleCardClick = () => {
+    setCards(shuffleArray([...cards]));
+  };
+
+  const renderCards = () => cards.map((card) => (
+    <Card name={card.name} key={card.id} handleClick={handleCardClick} />
+  ));
+
   return (
-    <div>Hello World</div>
+    <div>
+      {renderCards()}
+    </div>
   );
 }
 
