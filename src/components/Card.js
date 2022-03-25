9db84f39ead791245e9742c449ce2da3cf813bb6
sahaj <sahajjain13@gmail.com>
2022-03-25 23:34:29 +0530

    Add img attribute to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,17 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 
-function Card({ name, id, handleClick }) {
+function Card({
+  name, id, image, handleClick,
+}) {
   const handleCardClick = () => {
     handleClick(id);
   };
 
   return (
     <div className="card" onClick={handleCardClick}>
-      <div>
+      <img src={image} alt="character" />
+      <div className="name">
         {name}
       </div>
     </div>
